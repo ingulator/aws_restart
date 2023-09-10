c6eb24447b5addc214eee4680bb6c003ed10a195
Inga <ingulator@gmail.com>
2023-09-10 13:55:08 +0300

    Add files via upload

---
+++ numeric-data.py
@@ -0,0 +1,15 @@
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
